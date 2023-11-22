07a46a641466f34445ba4f40ad69799c1dbb39a7
Narayanareddy_Talakola@hotmail.com <Narayanareddy_Talakola@hotmail.com>
2023-11-22 20:55:21 +0000

    First commit to Git from workspace

---
+++ Test Notebook 2023-11-22 14_47_50.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ### My first notebok
+# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %python
+# MAGIC print("this is test")
+
+# COMMAND ----------
+
+
